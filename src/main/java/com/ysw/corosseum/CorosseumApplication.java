e57705030790cafa7ec794269f6eaa92434e811d
Seungwan Yoo <ysw99@kakao.com>
2025-12-10 21:02:48 +0900

    chore: initial project setup

---
+++ src/main/java/com/ysw/corosseum/CorosseumApplication.java
@@ -0,0 +1,13 @@
+package com.ysw.corosseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorosseumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorosseumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysw/corosseum/CorosseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysw.corosseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorosseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
