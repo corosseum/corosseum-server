2624cd9475ec31ed69b7b4d233cc90a89ca4ec19
Seungwan Yoo <ysw99@kakao.com>
2025-12-12 18:42:34 +0900

    refactor: Enum 타입에서 Swagger 어노테이션 제거 및 QuestStatus 값 변경

--- src/main/java/com/ysw/corosseum/domain/type/QuestStatus.java
+++ src/main/java/com/ysw/corosseum/domain/type/QuestStatus.java
@@ -1,14 +1,8 @@
 package com.ysw.corosseum.domain.type;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(
-	description = "퀘스트 상태: INACTIVE(비활성), ACTIVATED(활성화), DELETED(삭제됨)",
-	allowableValues = {"INACTIVE", "ACTIVATED", "DELETED"},
-	example = "ACTIVATED"
-)
 public enum QuestStatus {
-	INACTIVE,
+	PENDING,
 	ACTIVATED,
+	COMPLETED,
 	DELETED
 }
--- src/main/java/com/ysw/corosseum/domain/type/VoteType.java
+++ src/main/java/com/ysw/corosseum/domain/type/VoteType.java
@@ -1,12 +1,5 @@
 package com.ysw.corosseum.domain.type;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(
-	description = "투표 타입: DISGUSTING(🤮 토나와), GENIUS(🤩 천재다), LOL(🤣 빵터짐)",
-	allowableValues = {"DISGUSTING", "GENIUS", "LOL"},
-	example = "GENIUS"
-)
 public enum VoteType {
 	DISGUSTING,
 	GENIUS,
